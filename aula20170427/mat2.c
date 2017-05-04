329a9084a89475f9d9ed317ef5829ab7a0bbff9e
jessicavilaa <jessicavilaftp@hotmail.com>
2017-05-04 17:08:38 -0300

    Add files via upload

---
+++ aula20170427/mat2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+
+    double a, b, c, angulo;
+    printf("entre com o lado B ");
+    scanf("%lf", &b);
+    printf ("entre com o lado C ");
+    scanf ("%lf", &c);
+    printf ("entre com os angulos triangulo: ");
+    scanf ("%lf", &angulo);
+    a = sqrt ((b*b) + (c*c)- 2*(b*c)*cos(angulo));
+    printf ("%lf", a);
+    return EXIT_SUCCESS;
+
+}
