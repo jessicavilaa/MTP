23a7e409ecf286f8f948aaf3ddd4bd3d77fe3503
jessicavilaa <jessicavilaftp@hotmail.com>
2017-05-04 17:10:57 -0300

    Add files via upload

---
+++ aula20170427/mat3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double b, n, logaritmo;
+    printf("entre com a base do log: ");
+    scanf("%lf", &b);
+    printf("entre com o numero: ");
+    scanf("%lf", &n);
+    logaritmo= (log(n)/log(b));
+    printf("o logaritmo eh igual a: %lf\n", logaritmo);
+    return 0;
+}
